d3adfd9da58ffef8007a43b647ce5f54cdba0e74
Nemanja Veskovic <nemanja.veskovic@gmail.com>
2020-12-11 13:20:39 +0100

    update the example driver script

--- bb_scout.py
+++ bb_scout.py
@@ -1,24 +1,30 @@
-import sys
-from random import randint
+import bbapi_methods
 
-import requests
-import xmltodict, json
-from time import sleep
-from bb_api_scout import bbapi_methods
+#### READ ME ####
+#
+# This is the example driver script
+# You can create your own scripts by using this one as an example.
 
+# Steps needed:
+# 1. Login
+# 2. Do some actions
+# 3. Logout
 
 ###### Main actions ######
 username = "username here"
 bb_token = "bbapi token (access token) here"
+
+# 1. Login and get the seesion TODO: create an OOP aproach so that we can use bb_scout as an object
 s = bbapi_methods.login(username, bb_token)
 
+# 2. Do some actions
 leagues = bbapi_methods.get_list_of_league_ids(s, country_id=1, levels=[1, 2, 3, 4])
 teams = bbapi_methods.get_list_of_non_bot_teams(s, league_ids=leagues)
 
 # to filter by nationality IDs, you need to use a regular expression
 # example for Serbia (ID is 29):
 # players = bbapi_methods.get_list_of_players(
-#   s, team_ids=teams, age_pattern="{18,19}",
+#   s, team_ids=teams, age_pattern="18|19",
 #   min_potential=6,
 #   country_id_pattern="29",
 #   min_salary=0
@@ -40,12 +46,9 @@ players = bbapi_methods.get_list_of_players(
 
 
 
-# Logout
+# 3. Logout
 url = "http://bbapi.buzzerbeater.com/logout.aspx"
 s.get(url)
 
-for player in players:
-    print(player)
-print("Total number of players: {}".format(len(players)))
-
+# Additional: do some more actions like writing to a file or similar
 bbapi_methods.save_players_to_tsv_file(players=players, file_path="players2.tsv")
